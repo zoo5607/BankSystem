a249d34a9d698b2a3be85f726bff907c5eb7fcb0
ikhyeon kim <101814294+kimikhyeon1@users.noreply.github.com>
2022-11-24 14:43:29 +0900

    Update BankVO.java

--- BankSystem/src/main/java/BankSystem/BankVO.java
+++ BankSystem/src/main/java/BankSystem/BankVO.java
@@ -1,4 +1,6 @@
 package BankSystem;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public class BankVO {
@@ -6,57 +8,22 @@ public class BankVO {
     private String name; //이름
     private String accountNum; //계좌 번호
     private String balance;  //잔고
-    private Date transactionDate; // 거래 일자
-    private Date transactionTime; // 거래 시간
+    private LocalDate transactionDate; // 거래 일자
+    private LocalDateTime transactionTime; // 거래 시간
     private String isDeposit;  // 입금
     private String isWithdrawal; // 출금
     private String transactionAmount; // 거래 금액
     private String bankName; // 은행명
 
-    public BankVO(String name, String accountNum, String balance, String isDeposit) {
+    public BankVO(String name, String accountNum, String balance, String bankName) {
         this.name = name;
         this.accountNum = accountNum;
         this.balance = balance;
-        this.transactionDate = transactionDate;
-        this.transactionTime = transactionTime;
+        this.transactionDate = new Date;
+        this.transactionTime = new Date;
         this.isDeposit = this.isDeposit;
         this.isWithdrawal = isWithdrawal;
         this.transactionAmount = transactionAmount;
-        this.bankName = bankName;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAccountNum(String accountNum) {
-        this.accountNum = accountNum;
-    }
-
-    public void setBalance(String balance) {
-        this.balance = balance;
-    }
-
-    public void setTransactionDate(Date transactionDate) {
-        this.transactionDate = transactionDate;
-    }
-
-    public void setTransactionTime(Date transactionTime) {
-        this.transactionTime = transactionTime;
-    }
-
-    public void setIsDeposit(String isDeposit) {
-        this.isDeposit = isDeposit;
-    }
-
-    public void setIsWithdrawal(String isWithdrawal) {
-        this.isWithdrawal = isWithdrawal;
-    }
-
-    public void setTransactionAmount(String transactionAmount) {
-        this.transactionAmount = transactionAmount;
-    }
-
-    public void setBankName(String bankName) {
         this.bankName = bankName;
     }
 
@@ -64,36 +31,72 @@ public class BankVO {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getAccountNum() {
         return accountNum;
+    }
+
+    public void setAccountNum(String accountNum) {
+        this.accountNum = accountNum;
     }
 
     public String getBalance() {
         return balance;
     }
 
-    public Date getTransactionDate() {
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+
+    public LocalDate getTransactionDate() {
         return transactionDate;
     }
 
-    public Date getTransactionTime() {
+    public void setTransactionDate(LocalDate transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public LocalDateTime getTransactionTime() {
         return transactionTime;
+    }
+
+    public void setTransactionTime(LocalDateTime transactionTime) {
+        this.transactionTime = transactionTime;
     }
 
     public String getIsDeposit() {
         return isDeposit;
     }
 
+    public void setIsDeposit(String isDeposit) {
+        this.isDeposit = isDeposit;
+    }
+
     public String getIsWithdrawal() {
         return isWithdrawal;
+    }
+
+    public void setIsWithdrawal(String isWithdrawal) {
+        this.isWithdrawal = isWithdrawal;
     }
 
     public String getTransactionAmount() {
         return transactionAmount;
     }
 
+    public void setTransactionAmount(String transactionAmount) {
+        this.transactionAmount = transactionAmount;
+    }
+
     public String getBankName() {
         return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
     }
 
     @Override
