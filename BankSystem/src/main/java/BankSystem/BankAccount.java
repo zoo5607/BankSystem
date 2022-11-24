b54f4c145e3321296796ea2c2b6d7bddb2e71900
zoo5607 <83705371+zoo5607@users.noreply.github.com>
2022-11-24 16:02:09 +0900

    BankAccount 수정

--- BankSystem/src/main/java/BankSystem/BankAccount.java
+++ BankSystem/src/main/java/BankSystem/BankAccount.java
@@ -1,103 +1,138 @@
 package BankSystem;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
 public class BankAccount {
-    Scanner sc = new Scanner(System.in);
-    List<BankVO> bankusers = new ArrayList<>();
-    public void run() {
+    static Scanner sc = new Scanner(System.in);
+    static List<BankVO> bankusers = new ArrayList<>();
+
+    public void run() { // 시스템메뉴
 
         int key = 0;
-        while ((key = menu()) != 0) {
+        while ((key = systemmenu()) != 0) {
             switch (key) {
-                case 1: accountCreate();
+                case 1:
+                    accountCreate();
                     break;
-                case 2: accountManagement();
+                case 2:
+                    accountManagement();
                     break;
-                case 3: accountUpdate();
+                case 3:
+                    accountDeposit();
                     break;
-                case 4: accountDelete();
-                }
+                case 4:
+                    accountWithdrewal();
             }
         }
+    }
 
-
-    private void accountCreate() {
-            sc.nextLine();
-            String name = getStrInput("이름: ");
-            String bankName = getStrInput("은행명: ");
-            String accountNum = getStrInput("계좌번호: ");
-            String isDeposit = getStrInput("입금금액: ");
-
-            bankusers.add(new BankVO(name,bankName,accountNum,isDeposit));
-            System.out.println("계좌 등록 성공");
-            }
-
-    private void accountManagement() {
-            int key = 0;
-            while ((key = clientmenu()) != 0) {
-                switch (key) {
-                    case 1: clientManagement();
-                        break;
-                    case 2: clientList();
-                        break;
-                }
+    private void accountCreate() { //  은행 메뉴
+        int key = 0;
+        while ((key = bankmenu()) != 0) {
+            switch (key) {
+                case 1:
+                    Create();
+                    break;
+                case 2:
+                    Update();
+                    break;
+                case 3:
+                    Delete();
+                    break;
             }
         }
-        private void clientManagement() {
+    }
 
+    private void Create() { // 생성
+        sc.nextLine();
+        String name = getStrInput("이름: ");
+        String bankName = getStrInput("은행명: ");
+        String accountNum = getStrInput("계좌번호: ");
+
+        bankusers.add(new BankVO(name, bankName, accountNum));
+        System.out.println("계좌 등록 성공");
+    }
+
+    private void Update() { // 수정
+        if (bankusers.isEmpty()) {
+            System.out.println("고객명이 없습니다.");
+            return;
         }
-        private void clientList(){
-            Iterator<BankVO> ite = bankusers.iterator();
+        System.out.println("수정하실 고객명을 입력해주세요.");
+        Scanner scanner = new Scanner(System.in);
 
-            while (ite.hasNext()) {
-                System.out.println(ite.next() + " ");
-            }
-        }
+        String bankName = scanner.nextLine();
 
-        private void accountUpdate() {
-            if (bankusers.isEmpty()) {
-                System.out.println("고객명이 없습니다.");
-                return;
-            }
-            System.out.println("수정하실 고객명을 입력해주세요.");
-            Scanner scanner = new Scanner(System.in);
-
-            String bankName = scanner.nextLine();
-
-            for (int i =0; i<bankusers.size();i++){
-                if (bankName.equals(bankusers.get(i).getBankName())){
-                    System.out.println("계좌번호를 입력해 주세요");
-                    String accountNum = scanner.nextLine();
-                    if(accountNum.equals(bankusers.get(i).getAccountNum())){
-                        System.out.println("새로운 은행을 입력해 주세요");
-                        String newBank = scanner.nextLine();
-                        bankusers.get(i).setBankName(newBank);
-                        System.out.println("수정이 완료 되었습니다.");
-                        return;
-                    }else{
-                        System.out.println("계좌번호가 틀립니다.");
-                    }
-
-                }
-            }
-            System.out.println("회원 정보가 없습니다.");
-        }
-        private void accountDelete() {
-            sc.nextLine();
-            String name = getStrInput("삭제할 이름 :");
-
-            for(int i = 0; i<bankusers.size(); i++ ){
-                if(name == bankusers.get(i).getName()){
-                    bankusers.remove(i);
-                    System.out.println("삭제되었습니다.");
+        for (int i = 0; i < bankusers.size(); i++) {
+            if (bankName.equals(bankusers.get(i).getBankName())) {
+                System.out.println("계좌번호를 입력해 주세요");
+                String accountNum = scanner.nextLine();
+                if (accountNum.equals(bankusers.get(i).getAccountNum())) {
+                    System.out.println("새로운 은행을 입력해 주세요");
+                    String newBank = scanner.nextLine();
+                    bankusers.get(i).setBankName(newBank);
+                    System.out.println("수정이 완료 되었습니다.");
+                    return;
                 } else {
-                    System.out.println("정보와 맞지 않습니다.");
+                    System.out.println("계좌번호가 틀립니다.");
                 }
+
             }
         }
+        System.out.println("회원 정보가 없습니다.");
+    }
+
+    private void Delete() { // 삭제
+        sc.nextLine();
+        String name = getStrInput("삭제할 이름 :");
+
+        for (int i = 0; i < bankusers.size(); i++) {
+            if (name == bankusers.get(i).getName()) {
+                bankusers.remove(i);
+                System.out.println("삭제되었습니다.");
+            } else {
+                System.out.println("정보와 맞지 않습니다.");
+            }
+        }
+    }
+
+    private void accountManagement() { // 고객메뉴
+        int key = 0;
+        while ((key = clientmenu()) != 0) {
+            switch (key) {
+                case 1:
+                    clientManagement();
+                    break;
+                case 2:
+                    clientList();
+                    break;
+            }
+        }
+    }
+
+    private void clientManagement() { // 계좌 관리
+
+    }
+
+    private void clientList() { // 계좌 목록
+        Iterator<BankVO> ite = bankusers.iterator();
+
+        while (ite.hasNext()) {
+            System.out.println(ite.next() + " ");
+        }
+    }
+
+    private void accountDeposit() { // 예금
+
+    }
+
+    private void accountWithdrewal() { // 출금
+
+    }
+
     private String getStrInput(String s) {
         System.out.println(s);
         return sc.nextLine();
@@ -107,10 +142,18 @@ public class BankAccount {
         System.out.println(s);
         return sc.nextInt();
     }
-    private int menu() {
-        System.out.println("=========================Bank System===================");
-        return getNumInput("| 1.은행등록 | 2.은행관리 | 3.은행수정 | 4.은행삭제 | 0.종료 |");
+
+
+    private int systemmenu() {
+        System.out.println("=====================Bank System===================");
+        return getNumInput("| 1.은행등록 | 2.은행관리 | 3.예금 | 4.출금 | 0.종료 |");
     }
+
+    private int bankmenu() {
+        System.out.println("========Bank Update / Delete=========");
+        return getNumInput("| 1.등록 | 2.수정 | 3.삭제 | 0.종료 |");
+    }
+
     private int clientmenu() {
         System.out.println("=========Client Management=========");
         return getNumInput("| 1.계좌관리 | 2.계좌목록 | 0.종료 |");
